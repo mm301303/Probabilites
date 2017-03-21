d246d3641c8740cdef481764c0f962e9e86199d7
Maxime M <maxime.moineau@free.fr>
2017-03-21 09:14:04 +0100

    Ignoring is a morally weak choice

--- src/test/java/DS1Test.java
+++ src/test/java/DS1Test.java
@@ -36,7 +36,7 @@ public class DS1Test {
         System.out.println("\n");
     }
     @Test
-    @Ignore
+    
     //TODO fix
     public void Question3DS1Test() throws Exception, LawException, CalculationException {
         double res = 0;
