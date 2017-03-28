3143ec984c02ed84bf511243fef3ce9cfc8755cc
Maxime M <maxime.moineau@free.fr>
2017-03-28 23:37:02 +0200

    test failures to be fixed

--- src/main/java/Properties/Markov.java
+++ src/main/java/Properties/Markov.java
@@ -9,7 +9,8 @@ public class Markov {
      * @param esp_x
      * @return P(x<y)<this
      */
-    public static double P_x_inferieur_a_y(double esp_x, double y){
+    public static double P_x_sup_a_y_inferieur_a(double esp_x, double y){
         return esp_x/y;
     }
+
 }
