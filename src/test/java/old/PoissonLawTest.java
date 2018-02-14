8ca890636b7c27c99587348c5a49f1f79ee4a9c9
Maxime M <maxime.moineau@free.fr>
2018-02-14 14:45:18 +0100

    ahhh les test pourris -> on refait ça c'est le TD4

--- src/test/java/old/PoissonLawTest.java
+++ src/test/java/old/PoissonLawTest.java
@@ -22,16 +22,5 @@ public class PoissonLawTest {
         poisson = new PoissonLaw(5.);
         assertEquals("Poisson Laws.functions.Law", poisson.getName());
     }
-    @Test
-    public void exo33TD1(){
-        System.out.println("\nexo33TD1");
-        System.out.println("\nexo sur le nombre d'avions dans un aéroport pour une loi de poisson de paramètre lambda");
-        System.out.println("on veut savoir la proba d'avoir moins de 3 avions en un jour");
-        lambda = 3;
-        poisson = new PoissonLaw(3);
-        double res = 1.- poisson.getProbabiliteDeX(2)-poisson.getProbabiliteDeX(1) - poisson.getProbabiliteDeX(0);
-        System.out.println("1-poisson.getProbabiliteDeX(2)-poisson.getProbabiliteDeX(1) = " + res);
-        assertEquals(0.5768, res, delta);
-    }
 
 }
--- src/test/java/old/UniformLawTest.java
+++ src/test/java/old/UniformLawTest.java
@@ -11,41 +11,6 @@ import static org.junit.Assert.*;
  */
 public class UniformLawTest {
     double delta = Calculation.DELTA;
-    @Test
-    public void exo1TD1(){//corrected
-        System.out.println(this.getClass().getSimpleName()+" exo1TD1");
-        double borneInfU = 0;
-        double borneSuppU = 30;
 
-        UniformLaw uniformLaw = new UniformLaw(borneInfU, borneSuppU);
-
-        System.out.println("Attendre moins de 5 min :");
-        double res5 = uniformLaw.F(10,15) + uniformLaw.F(25,30);
-        System.out.println("uniformLaw.F(10,15) + uniformLaw.F(25,30) = " + res5);
-        assertEquals(1./3., res5,0.01);
-
-        System.out.println("Attendre moins de 10 min :");
-        double res5_2 = uniformLaw.F(5,10) + uniformLaw.F(20,25);
-        System.out.println("uniformLaw.F(5,10) + uniformLaw.F(20,25) = " + res5_2);
-        assertEquals(1./3., res5_2,0.01);
-        System.out.println("resultat r1+r2 = " + (double)(res5_2+res5));
-
-        assertEquals(2./3., res5+res5_2,0.01);
-
-    }
-
-    @Test
-    public void exo2TD1() {
-        System.out.println("exo2TD1");
-        double et =  Math.sqrt(48.0);
-        double inf = 4.0 - et/2;
-        double supp = 4.0 + et/2;
-        UniformLaw ul = new UniformLaw(inf,supp);
-        System.out.println("ul.getEsperance() = " + ul.getEsperance());
-        assertEquals(4., ul.getEsperance(), 0.01);
-        System.out.println("ul.getVariance() = " + ul.getVariance());
-        assertEquals(4., ul.getVariance(), 0.01);
-
-    }
 
 }
